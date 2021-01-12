e6e004adedf03adba2b08aa905a1fa084abc522f
Michal <mkolodziejczyk269@gmail.com>
2021-01-12 21:25:57 +0100

    Create confirm page

---
+++ src/pages/ConfirmPage/ConfirmPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ConfirmPage.css";
+
+const ConfirmPage = () => {
+  return (
+    <div>
+      <h1>Cnfirm page</h1>
+    </div>
+  );
+};
+
+export default ConfirmPage;
