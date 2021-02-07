535c4cf9aeed0e12e88695a076d43aaab4f8b754
Michal <mkolodziejczyk269@gmail.com>
2021-02-07 21:56:20 +0100

    Modify AuthorPage

--- src/pages/AuthorPage/AuthorPage.js
+++ src/pages/AuthorPage/AuthorPage.js
@@ -12,16 +12,47 @@ const AuthorPage = props => {
   useEffect(() => {
     if (props.location.author) {
       setState(props.location.author);
+    } else if (author.id === id) {
+      setState(author);
+    } else {
+      getAuthor(id);
     }
 
     // eslint-disable-next-line
   }, []);
 
-  return (
-    <div className="authorPage">
-      <h2>author</h2>
-    </div>
-  );
+  useEffect(() => {
+    if (author.id === id) {
+      setState(author);
+    }
+
+    // eslint-disable-next-line
+  }, [author]);
+
+  if (Object.keys(state).length === 0) {
+    return (
+      <div className="authorPage__container">
+        <div className="authorPage__containerSpinner text-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="authorPage container-xl">
+        <h1 className="authorPage__name">{`${state.firstName} ${state.lastName}`}</h1>
+        <p className="authorPage__birthDate">
+          Born: {format(new Date(state.birthDate), "yyyy-MM-dd")}
+        </p>
+        <p className="authorPage__description">{state.description}</p>
+        <div className="authorPage__booksContainer">
+          <p>Books</p>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default AuthorPage;
