1f48441abec1128a1b39df6dd954808664eb6a87
Michal <mkolodziejczyk269@gmail.com>
2021-03-02 20:40:59 +0100

    BookPage css

--- src/pages/BookPage/BookPage.js
+++ src/pages/BookPage/BookPage.js
@@ -83,22 +83,24 @@ const BookPage = props => {
             <p className="bookPage__contentHeaderTitle">{state.title}</p>
             <span className="bookPage__contentHeaderStatus">Available</span>
           </div>
-          <Link
-            to={{ pathname: `/author/${state.authorID}` }}
-            className="bookPage__contentAuthor"
-          >
-            {state.authorName}
-          </Link>
-          <p className="bookPage__contentPublisher">{state.publisher}</p>
-          <p className="bookPage__contentCategory">{state.category}</p>
-          <p className="bookPage__contentPublishedDate">
-            {format(new Date(state.publishedDate), "yyyy-MM-dd")}
-          </p>
-          <p className="bookPage__contentLanguage">
-            Language: {state.language}
-          </p>
-          <p className="bookPage__contentPages">{state.totalPages} pages</p>
-          <p className="bookPage__contentDescription">{state.description} </p>
+          <div className="bookPage__contentBody">
+            <Link
+              to={{ pathname: `/author/${state.authorID}` }}
+              className="bookPage__contentAuthor"
+            >
+              {state.authorName}
+            </Link>
+            <p className="bookPage__contentPublisher">{state.publisher}</p>
+            <p className="bookPage__contentCategory">{state.category}</p>
+            <p className="bookPage__contentPublishedDate">
+              {format(new Date(state.publishedDate), "yyyy-MM-dd")}
+            </p>
+            <p className="bookPage__contentLanguage">
+              Language: {state.language}
+            </p>
+            <p className="bookPage__contentPages">{state.totalPages} pages</p>
+            <p className="bookPage__contentDescription">{state.description} </p>
+          </div>
           {error ? (
             <span className="bookPage__contentError">
               Already added to basket!
